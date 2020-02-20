ae6f586db6090ebf5828e63e067863248fe4bb57
marco-explorabiotech <m.amato@explora-biotech.com>
2020-02-20 17:57:05 +0100

    updates dopo i feedback di maria

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Tabae
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'it'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
